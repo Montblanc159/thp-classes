f30beaae970efc791f73ae66c434b6631661ed38
Montblanc159 <guillaume.zaugg@gmail.com>
2019-01-21 16:38:06 +0100

    event.rb - class base completed

--- lib/event.rb
+++ lib/event.rb
@@ -3,5 +3,19 @@
 require 'rb-readline'
 require 'pry'
 require 'dotenv'
+require 'time'
+
 
 Dotenv.load
+
+
+class Event
+  attr_accessor :start_date, :length, :title, :attendees
+
+  def initialize(start_date, length, title, attendees)
+    @length = length
+    @title = title
+    @attendees = attendees
+    @start_date = Time.parse(start_date)
+  end
+end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal : true
 
 require_relative '../lib/event.rb'
+
+describe Event do
+
+  before(:each) do
+    Object.send(:remove_const, 'Event')
+    load 'event.rb'
+  end
+
+  describe "initializer" do
+    it "creates a new event" do
+      event = Event.new("2019-01-21 09:10", 30, "event", ["attendee_1@gmail.com", "attendee_2@gmail.com"])
+      expect(event.class).to eq(Event)
+    end
+
+    it "saves date as Time" do
+      event = Event.new("2019-01-21 09:10", 30, "event", ["attendee_1@gmail.com", "attendee_2@gmail.com"])
+      expect(event.start_date.class).to eq(Time)
+    end
+
+    it "saves the title" do
+      event = Event.new("2019-01-21 09:10", 30, "event", ["attendee_1@gmail.com", "attendee_2@gmail.com"])
+      expect(event.title).to eq("event")
+    end
+
+    it "saves the attendees" do
+      event = Event.new("2019-01-21 09:10", 30, "event", ["attendee_1@gmail.com", "attendee_2@gmail.com"])
+      expect(event.attendees.length).to eq(2)
+      expect(event.attendees).to eq(["attendee_1@gmail.com", "attendee_2@gmail.com"])
+    end
+  end
+end
